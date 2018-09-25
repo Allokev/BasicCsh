ca139ff7bef49236f9def9f198d5eff7bb66b332
Алексей Локтев <alexloktev96@gmail.com>
2018-09-25 22:44:24 +0300

    Площадь куга по радиусу

---
+++ HW3/HW3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double p = 3.14;
+           
+            Console.WriteLine("Введите радиус круга");
+            double r = Convert.ToDouble(Console.ReadLine());
+
+            double s = p * (r * r);
+            Console.WriteLine("Площадь круга равна:" + s);
+            Console.ReadLine();
+
+        }
+    }
+}
