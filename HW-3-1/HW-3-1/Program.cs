2f53a443b9931373e9adaa6d2e30e73cc9f75e0d
Алексей Локтев <alexloktev96@gmail.com>
2018-09-25 21:23:29 +0300

    Practice-1

---
+++ HW-3-1/HW-3-1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_3_1
+{
+    class Program
+    {
+        static void Summ()
+        {
+
+            Console.WriteLine("Введите первое число:");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число:");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("В сумме:" + (x + y));
+            Console.Read();
+
+        }
+        static void Main(string[] args)
+        {
+            Summ();
+        }
+    }
+}
