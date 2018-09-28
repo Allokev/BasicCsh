90a3f62ab3a8f11e4a6dcae05ed754421786835f
Алексей Локтев <alexloktev96@gmail.com>
2018-09-28 20:22:44 +0300

    Switch

---
+++ Lesson- 4-pr-2/Lesson- 4-pr-2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson__4_pr_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char x;
+            Console.WriteLine("Введите символ:");
+            x = Convert.ToChar(Console.Read());
+
+            switch (x)
+            {
+                case 'w':
+                    Console.WriteLine("Сдвиньте вверх");
+                    Console.ReadLine();
+                    break;
+                    
+
+                case 'a':
+                    Console.WriteLine("Сдвиньте влево");
+                    Console.ReadLine();
+                    break;
+
+                case 's':
+                    Console.WriteLine("Сдвиньте вниз");
+                    Console.ReadLine();
+                    break;
+
+                case 'd':
+                    Console.WriteLine("Сдвиньте вправо");
+                    Console.ReadLine();
+                    break;
+
+                default:
+                    Console.WriteLine("Нет необходимости в перемещении");
+                    Console.ReadLine();
+                    break;
+                    
+
+            }
+            
+
+
+
+
+        }
+    }
+}
