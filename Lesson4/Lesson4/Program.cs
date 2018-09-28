0ecadfeefad9deeab50f0a0fe7f7b0da467840ab
Алексей Локтев <alexloktev96@gmail.com>
2018-09-28 19:16:59 +0300

    New commit

--- Lesson4/Lesson4/Program.cs
+++ Lesson4/Lesson4/Program.cs
@@ -10,7 +10,7 @@ namespace Lesson4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("New branch");
+            Console.WriteLine("Hello World!");
         }
     }
 }
