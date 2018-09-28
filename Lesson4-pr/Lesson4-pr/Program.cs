34e0a86e9f1faf39103c5ce05d5b30316cb42da9
Алексей Локтев <alexloktev96@gmail.com>
2018-09-28 19:59:50 +0300

    Practice 4

---
+++ Lesson4-pr/Lesson4-pr/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4_pr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое значение:");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Введите второе значение:");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            if (y >= 0 & x >= 0)
+            {
+
+                if (x == y)
+                {
+                    Console.WriteLine("Значеня равны");
+                }
+                if (x > y)
+                {
+                    Console.WriteLine("Значение x больше y на:" + (x - y));
+                }
+                if (x < y)
+                {
+                    Console.WriteLine("Значение x меньше y на:" + (y - x));
+                }
+            }
+            else
+            {
+                Console.WriteLine("Отрицательное значение ");
+
+                Console.ReadLine();
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
