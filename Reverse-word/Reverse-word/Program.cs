52957de32981eb22450d3616b1956c9fb00dc360
Алексей Локтев <alexloktev96@gmail.com>
2018-09-29 19:48:15 +0300

    Word reverse

---
+++ Reverse-word/Reverse-word/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово:");
+            string word = Console.ReadLine();
+            
+            for (int number = word.Length-1; number >= 0; number--)
+            {
+                Console.Write(word[number]);
+                
+            }
+           
+            Console.ReadLine();
+        }
+    }
+}
