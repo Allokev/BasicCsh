5f2c0f743f94766049a518b71babdf93c998cad8
Алексей Локтев <alexloktev96@gmail.com>
2018-09-29 17:11:20 +0300

    Lettser`s 'A' in word

---
+++ HW-4-Word`s counting/HW-4-Word`s counting/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_4_Word_s_counting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово:");
+            string word = Console.ReadLine();
+            int count = 0;
+            for(int x = 0; word.Length > x; x++)
+            {
+                if (word[x] == 'a'| word[x]=='а')
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine("Количесвто букв А в слове {0} равно: {1}", word, count);
+            Console.ReadLine();
+        }
+    }
+}
