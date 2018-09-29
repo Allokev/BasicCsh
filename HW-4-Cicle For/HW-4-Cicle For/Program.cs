2c0074248ecd28b45efddecace1cd348e96fe99d
Алексей Локтев <alexloktev96@gmail.com>
2018-09-29 14:25:39 +0300

    Cycle For from 10 to 0

---
+++ HW-4-Cicle For/HW-4-Cicle For/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_4_Cicle_For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x = 10; x >= 0; x--)
+            {
+                if (x % 2 == 0)
+                {
+                    if (x == 0)
+                    {
+                        Console.WriteLine(x+" - Это ноль");
+                    }
+                    else
+                    {
+                        Console.WriteLine(x + " - Четное");
+                    }
+                    
+                    
+                }
+                else
+                {
+                    Console.WriteLine(x + " - Нечетное");
+                }
+                
+               
+                
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
