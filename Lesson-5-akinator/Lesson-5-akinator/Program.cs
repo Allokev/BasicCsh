37b10cb75cfea8dde1da8f1964b354e5960d9c45
Алексей Локтев <alexloktev96@gmail.com>
2018-10-03 13:45:53 +0300

    Акинатор и степень

---
+++ Lesson-5-akinator/Lesson-5-akinator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_5_akinator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int middle = 50;
+            int middleH = 25;
+            
+
+            
+
+            while (true)
+            {
+                Console.WriteLine("Если ваше число больше {0}, поставьте >,если меньше - <, если равно - =", middle);
+                char input = Convert.ToChar(Console.ReadLine());
+
+                if (input == '>')
+                {
+                    middle = middle + middleH;
+                }
+                else if(input == '<')
+                {
+                    middle = middle - middleH;
+                }
+                else if(input == '=')
+                {
+                    Console.WriteLine("Ваше число {0}", middle);
+                }
+                middleH = middleH / 2 + 1;
+                
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Lesson-5-while-new/Lesson-5-while-new/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_5_while_new
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите степень:");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите число для степени:");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            int r = 1;
+            int count = 1;
+
+            do
+            {
+
+                r = r * number2;
+
+            } while (count++ < number1);
+            Console.WriteLine("Результат: " + r);
+            Console.ReadLine();
+        }
+    }
+}
