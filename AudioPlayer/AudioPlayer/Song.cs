81290d028f9b16eba7fbb7886fca2553b98dd745
Алексей Локтев <alexloktev96@gmail.com>
2018-10-05 19:50:51 +0300

    Audioplayer

---
+++ AudioPlayer/AudioPlayer/Song.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioPlayer
+{
+    class Song
+    {
+        string Name;
+        Artist artistOfCurrentSOng;
+        Genre genreOfCurrentSong;
+        int duration;
+        
+    }
+}
