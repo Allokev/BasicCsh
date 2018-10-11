665c88e558fd61ad36d942cb25ab1adf21af6660
Алексей Локтев <alexloktev96@gmail.com>
2018-10-12 00:30:40 +0300

    Strings

---
+++ Sentense-Trim/Sentense-Trim/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sentense_Trim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение:");
+            string sentense = Console.ReadLine();
+            int number = sentense.Length;
+            if (number > 13)
+                sentense = sentense.Remove(13);
+
+            Console.WriteLine(sentense+"...");
+            Console.ReadLine();
+                    
+        }
+    }
+}
---
+++ String-Replace/String-Replace/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Replace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как насчет стихотворения?");
+            string poem = Console.ReadLine();
+            
+
+            var case1 = poem.Replace("о", "а");
+            var case2 = case1.Replace("л", "ль");
+            var case3 = case2.Replace("о", "а");
+
+            string[] poemArray = case3.Split(',');
+            foreach (var item in poemArray)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
