f794ca5d118d558e625ed5a79fe034d4eb4e0163
Алексей Локтев <alexloktev96@gmail.com>
2018-10-22 01:04:07 +0300

    Coordinates and datetime

---
+++ Coordinates/Coordinates/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coordinates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Coordinates coord = new Coordinates();
+
+            Console.Write("Введите координату x: ");
+            double.TryParse(Console.ReadLine(),out coord.x);
+
+            Console.Write("Введите координату y: ");
+            double.TryParse(Console.ReadLine(), out coord.y);
+
+            Console.Write("Введите координату z: ");
+            double.TryParse(Console.ReadLine(), out coord.z);
+            var (x, y, z) = coord;
+            Console.WriteLine($"Длина вектора равна: {Math.Sqrt(Math.Pow(x,2)+Math.Pow(y,2)+Math.Pow(z,2))}");
+            Console.ReadLine();
+        }
+        struct Coordinates
+        {
+            public double x;
+            public double y;
+            public double z;
+
+            public void Deconstruct(out double X, out double Y, out double Z)
+            {
+               X = this.x;
+               Y = this.y;
+               Z = this.z;
+            }
+        }
+    }
+}
---
+++ Date-deconst/Date-deconst/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Date_deconst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var date1 = DateTime.Now;
+
+            var (_, _, _, hours, minutes, seconds) = date1;
+
+
+            Console.WriteLine($"{hours}:{minutes}:{seconds}");
+            
+        }
+        public  class DateTimeToday
+        {
+            public  void Deconstruct(this DateTime date,out int yr, out int mnh, out int dy, out int hrs, out int min, out int sec)
+            {
+                yr = date.Year;
+                mnh = date.Month;
+                dy = date.Day;
+                hrs = date.Hour;
+                min = date.Minute;
+                sec = date.Second;
+            }
+        }
+        
+        
+        
+        
+    }
+    
+}
